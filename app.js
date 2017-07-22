9f495de2cf0a3a731d8860e362cf77bf9f705ac4
mdemuynck <maarten.demuynck@loqutus.com>
2017-07-22 21:56:13 +0200

    Fixing child exec error function

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/station/:number', function(req, res){
 
 
 function irsend(cmd){
-    child.exec('irsend SEND_ONCE Philips ' + cmd, (err, stdout, stderr) => {
+    child.exec('irsend SEND_ONCE Philips ' + cmd, function(err, stdout, stderr){
 		if (err){
 			console.log('exec error: ' + JSON.stringify(err));
 			return;
