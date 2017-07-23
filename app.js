2007f5695e39421a9f8c8766fe8c3b9e7f0a5781
mdemuynck <maarten.demuynck@loqutus.com>
2017-07-23 21:29:22 +0200

    remove use of variable

--- app.js
+++ app.js
@@ -50,16 +50,16 @@ app.get('/rvdown', function(req, res){
 app.get('/station/:number', function (req, res) {
     console.log("auxenabled: " + auxenabled);
     console.log("stream: " + stream);
-    if (auxenabled) {
+    //if (auxenabled) {
         //stream from the net
         internetradio.getStationInfo(stream, function (error, station) {
             console.log(station);
         });
-    } else {
+    /*} else {
         //play FM
         irsend('KEY_' + req.params.number);
         res.sendFile('index.html', { root: __dirname });
-    }
+    }*/
 });
 
 
